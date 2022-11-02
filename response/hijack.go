925b38e77828f35baef41f45290fa9aee4ce3548
Octavian <octavr@gmail.com>
2022-11-02 21:43:40 +0100

    added NewHttpHijackConnectionResponse

---
+++ response/hijack.go
@@ -0,0 +1,40 @@
+package response
+
+import (
+	"bufio"
+	"errors"
+	"github.com/ixtendio/gofre/request"
+	"net"
+	"net/http"
+)
+
+type HttpHijackConnectionResponse struct {
+	hjCallbackFunc func(net.Conn, *bufio.ReadWriter, error)
+}
+
+// NewHttpHijackConnectionResponse creates a new HttpResponse that hijack the current TCP connection, giving the full access of it to the application
+// This response might be useful in websockets implementations, for example
+func NewHttpHijackConnectionResponse(hijackCallbackFunc func(net.Conn, *bufio.ReadWriter, error)) *HttpHijackConnectionResponse {
+	return &HttpHijackConnectionResponse{hjCallbackFunc: hijackCallbackFunc}
+}
+
+func (r *HttpHijackConnectionResponse) StatusCode() int {
+	return 0
+}
+
+func (r *HttpHijackConnectionResponse) Headers() http.Header {
+	return nil
+}
+
+func (r *HttpHijackConnectionResponse) Cookies() HttpCookies {
+	return nil
+}
+
+func (r *HttpHijackConnectionResponse) Write(w http.ResponseWriter, req *request.HttpRequest) error {
+	if hj, ok := w.(http.Hijacker); ok {
+		r.hjCallbackFunc(hj.Hijack())
+	} else {
+		r.hjCallbackFunc(nil, nil, errors.New("the current http.ResponseWriter doesn't support hijack functionality"))
+	}
+	return nil
+}
---
+++ response/hijack_test.go
@@ -0,0 +1,116 @@
+package response
+
+import (
+	"bufio"
+	"net"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+var tcpCon = &net.TCPConn{}
+var rw = &bufio.ReadWriter{}
+
+type fakeHijackResponseWriter struct {
+}
+
+func (fakeHijackResponseWriter) Header() http.Header                          { return nil }
+func (fakeHijackResponseWriter) Write([]byte) (int, error)                    { return 0, nil }
+func (fakeHijackResponseWriter) WriteHeader(statusCode int)                   {}
+func (fakeHijackResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) { return tcpCon, rw, nil }
+
+type fakeNonHijackResponseWriter struct {
+}
+
+func (fakeNonHijackResponseWriter) Header() http.Header        { return nil }
+func (fakeNonHijackResponseWriter) Write([]byte) (int, error)  { return 0, nil }
+func (fakeNonHijackResponseWriter) WriteHeader(statusCode int) {}
+
+func TestHttpHijackConnectionResponse_Write(t *testing.T) {
+	var gotCon net.Conn
+	var gotRw *bufio.ReadWriter
+	var gotErr error
+
+	type want struct {
+		con net.Conn
+		rw  *bufio.ReadWriter
+	}
+	type args struct {
+		w              http.ResponseWriter
+		hjCallbackFunc func(net.Conn, *bufio.ReadWriter, error)
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    want
+		wantErr bool
+	}{
+		{
+			name: "HijackResponseWriter",
+			args: args{
+				w: fakeHijackResponseWriter{},
+				hjCallbackFunc: func(conn net.Conn, writer *bufio.ReadWriter, err error) {
+					gotCon = conn
+					gotRw = writer
+					gotErr = err
+				},
+			},
+			want: want{
+				con: tcpCon,
+				rw:  rw,
+			},
+			wantErr: false,
+		},
+
+		{
+			name: "NonHijackResponseWriter",
+			args: args{
+				w: fakeNonHijackResponseWriter{},
+				hjCallbackFunc: func(conn net.Conn, writer *bufio.ReadWriter, err error) {
+					gotCon = conn
+					gotRw = writer
+					gotErr = err
+				},
+			},
+			want: want{
+				con: nil,
+				rw:  nil,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := NewHttpHijackConnectionResponse(tt.args.hjCallbackFunc)
+			if err := r.Write(tt.args.w, nil); err != nil {
+				t.Fatalf("Write() returned error: %v", err)
+			}
+			if r.StatusCode() != 0 {
+				t.Errorf("StatusCode() expected value 0 got: %v", r.StatusCode())
+			}
+			if r.Headers() != nil {
+				t.Errorf("Headers() expected value nil got: %v", r.Headers())
+			}
+			if r.Cookies() != nil {
+				t.Errorf("Cookies() expected value nil got: %v", r.Cookies())
+			}
+			if tt.wantErr {
+				if gotErr == nil {
+					t.Errorf("Write() want error but got null")
+				}
+			} else {
+				if gotErr != nil {
+					t.Errorf("Write() error = %v, wantErr %v", gotErr, tt.wantErr)
+				}
+				got := want{
+					con: gotCon,
+					rw:  gotRw,
+				}
+				if !reflect.DeepEqual(tt.want, got) {
+					t.Errorf("Write() got: %v, want: %v", got, tt.want)
+				}
+			}
+
+		})
+	}
+}
