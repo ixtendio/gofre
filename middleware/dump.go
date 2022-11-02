f9bae776c6b4bf128ff70c8dc165b7a1e48818c2
Octavian <octavr@gmail.com>
2022-11-02 21:43:56 +0100

    added RequestDumper

--- middleware/dump.go
+++ middleware/dump.go
@@ -2,20 +2,71 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/ixtendio/gofre/handler"
 	"github.com/ixtendio/gofre/request"
 	"github.com/ixtendio/gofre/response"
-	"strings"
+	"time"
+	"unsafe"
 )
 
-// RequestDumper dumps the request (before processing) and the corresponding response.
+// RequestDumper dumps the request (before processing) and the corresponding response in JSON format.
 // It is especially useful in debugging problems.
-func RequestDumper(logResponsePayload bool, logger func(val string)) Middleware {
+func RequestDumper(logger func(val string)) Middleware {
 	return func(handler handler.Handler) handler.Handler {
 		return func(ctx context.Context, req *request.HttpRequest) (resp response.HttpResponse, err error) {
-			var sb strings.Builder
+			startTime := time.Now().UnixMilli()
+			// request logging
+			requestMap := map[string]any{}
+			requestMap["method"] = req.R.Method
+			requestMap["protocolVersion"] = req.R.Proto
+			requestMap["requestURI"] = req.R.RequestURI
+			requestMap["url"] = req.R.URL.String()
+			requestMap["remoteAddress"] = req.R.RemoteAddr
+			requestMap["transferEncoding"] = req.R.TransferEncoding
+			requestMap["contentLength"] = req.R.ContentLength
+			requestMap["host"] = req.R.Host
+			requestMap["isSecure"] = req.R.TLS != nil
+			requestMap["header"] = req.R.Header
+			requestMap["formFields"] = req.R.Form
+			var cookies []string
+			for _, c := range req.R.Cookies() {
+				if c != nil {
+					cookies = append(cookies, c.String())
+				}
+			}
+			if len(cookies) > 0 {
+				requestMap["cookies"] = cookies
+			}
+
 			resp, err = handler(ctx, req)
-			logger(sb.String())
+			if err != nil {
+				return nil, err
+			}
+
+			// response logging
+			responseMap := map[string]any{}
+			responseMap["statusCode"] = resp.StatusCode()
+			responseMap["header"] = resp.Headers()
+			cookies = nil
+			for _, c := range resp.Cookies() {
+				cookies = append(cookies, c.String())
+			}
+			if len(cookies) > 0 {
+				responseMap["cookies"] = cookies
+			}
+
+			data, err := json.Marshal(map[string]any{
+				"startTimeMs": startTime,
+				"endTimeMs":   time.Now().UnixMilli(),
+				"request":     requestMap,
+				"response":    responseMap,
+			})
+			if err != nil {
+				return nil, fmt.Errorf("failed to dump the request, err: %w", err)
+			}
+			logger(*(*string)(unsafe.Pointer(&data)))
 			return resp, err
 		}
 	}
---
+++ middleware/dump_test.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"bytes"
+	"context"
+	"github.com/ixtendio/gofre/request"
+	"github.com/ixtendio/gofre/response"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestRequestDumper(t *testing.T) {
+	r, _ := http.NewRequest("POST", "https://www.domain.com?qParam1=qVal1", bytes.NewBufferString(""))
+	r.Form = url.Values{"param1": {"val1.1", "val1.2"}, "param2": {"val2.1", "val2.2"}}
+	r.ContentLength = 256
+	r.Header = http.Header{"h1": {"hv1.1"}, "h2": {"hv2.1", "hv2.2"}}
+	r.RemoteAddr = "175.123.23.1"
+	r.AddCookie(&http.Cookie{
+		Name:  "Cookie1",
+		Value: "CookieVal1",
+	})
+	var result string
+	type args struct {
+		req    *http.Request
+		logger func(val string)
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "call",
+			args: args{
+				req: r,
+				logger: func(val string) {
+					result = val
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := RequestDumper(tt.args.logger)(func(ctx context.Context, r *request.HttpRequest) (response.HttpResponse, error) {
+				return response.PlainTextHttpResponseOK(""), nil
+			})(context.Background(), request.NewHttpRequest(tt.args.req))
+			if err != nil {
+				t.Fatalf("RequestDumper() got err: %v", err)
+			}
+			if len(result) == 0 {
+				t.Errorf("RequestDumper() got: %v, want not empty", result)
+			}
+		})
+	}
+}
